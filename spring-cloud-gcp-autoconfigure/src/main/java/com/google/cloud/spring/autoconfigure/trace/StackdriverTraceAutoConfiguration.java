95ba373b40bc0c6e7fbb4a273540706190c113d3
Mike Eltsufin <meltsufin@google.com>
2020-12-02 23:23:36 -0500

    wip fix up sample

--- spring-cloud-gcp-autoconfigure/src/main/java/com/google/cloud/spring/autoconfigure/trace/StackdriverTraceAutoConfiguration.java
+++ spring-cloud-gcp-autoconfigure/src/main/java/com/google/cloud/spring/autoconfigure/trace/StackdriverTraceAutoConfiguration.java
@@ -54,7 +54,6 @@ import zipkin2.reporter.stackdriver.StackdriverEncoder;
 import zipkin2.reporter.stackdriver.StackdriverSender;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -277,6 +276,4 @@ public class StackdriverTraceAutoConfiguration {
 			return builder -> builder.clientRequestParser(stackdriverHttpRequestParser);
 		}
 	}
-
-
 }
